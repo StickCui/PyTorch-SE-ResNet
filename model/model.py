eb35aaf028e486d7eef657a6c777ea9c51bcd3ed
StickCui <StickCui@users.noreply.github.com>
2018-05-29 16:41:44 +0800

    Fix a mistake for ResNeXt

--- model/model.py
+++ model/model.py
@@ -1,5 +1,6 @@
 # encoding:utf-8
 # Modify from torchvision
+# ResNeXt: Copy from https://github.com/last-one/tools/blob/master/pytorch/SE-ResNeXt/SeResNeXt.py
 import torch.nn as nn
 import math
 
@@ -117,27 +118,47 @@ class SEResNet(nn.Module):
         return x
 
 
-class BottleneckX(nn.Module):
-    expansion = 2
+class Selayer(nn.Module):
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BottleneckX, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 2, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 2)
+    def __init__(self, inplanes):
+        super(Selayer, self).__init__()
+        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
+        self.conv1 = nn.Conv2d(inplanes, inplanes / 16, kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(inplanes / 16, inplanes, kernel_size=1, stride=1)
         self.relu = nn.ReLU(inplace=True)
-        # SE
-        self.global_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv_down = nn.Conv2d(
-            planes * 2, planes // 8, kernel_size=1, bias=False)
-        self.conv_up = nn.Conv2d(
-            planes // 8, planes * 2, kernel_size=1, bias=False)
-        self.sig = nn.Sigmoid()
-        # Downsample
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+
+        out = self.global_avgpool(x)
+
+        out = self.conv1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.sigmoid(out)
+
+        return x * out
+
+
+class BottleneckX(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, cardinality, stride=1, downsample=None):
+        super(BottleneckX, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes * 2, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes * 2)
+
+        self.conv2 = nn.Conv2d(planes * 2, planes * 2, kernel_size=3, stride=stride,
+                               padding=1, groups=cardinality, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes * 2)
+
+        self.conv3 = nn.Conv2d(planes * 2, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+
+        self.selayer = Selayer(planes * 4)
+
+        self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
 
@@ -155,42 +176,44 @@ class BottleneckX(nn.Module):
         out = self.conv3(out)
         out = self.bn3(out)
 
-        out1 = self.global_pool(out)
-        out1 = self.conv_down(out1)
-        out1 = self.relu(out1)
-        out1 = self.conv_up(out1)
-        out1 = self.sig(out1)
+        out = self.selayer(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
 
-        res = out1 * out + residual
-        res = self.relu(res)
+        out += residual
+        out = self.relu(out)
 
-        return res
+        return out
 
 
 class SEResNeXt(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000):
-        self.inplanes = 64
+    def __init__(self, block, layers, cardinality=32, num_classes=1000):
         super(SEResNeXt, self).__init__()
+        self.cardinality = cardinality
+        self.inplanes = 64
+
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 128, layers[0])
-        self.layer2 = self._make_layer(block, 256, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 512, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 1024, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7)
-        self.fc = nn.Linear(1024 * block.expansion, num_classes)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
@@ -205,10 +228,10 @@ class SEResNeXt(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers.append(block(self.inplanes, planes, self.cardinality, stride, downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+            layers.append(block(self.inplanes, planes, self.cardinality))
 
         return nn.Sequential(*layers)
 
@@ -225,6 +248,7 @@ class SEResNeXt(nn.Module):
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
+
         x = self.fc(x)
 
         return x
